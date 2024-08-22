54ff2f846ceada35bb5546aa4f19dc2f4ba78353
Kaif Shakeel <95091483+KaifShakeel76@users.noreply.github.com>
2024-08-22 11:50:47 +0530

    Create main.tf

---
+++ Redis/redis/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "redis_server" {
+  ami           = "ami-0a0e5d9c7acc336f1"  # Replace with an appropriate AMI ID for Redis
+  instance_type = var.instance_type
+  subnet_id     = element(var.subnet_ids, 0)
+
+  tags = {
+    Name = "Redis-Server-${var.environment}"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y redis${var.redis_version}
+              systemctl start redis
+              systemctl enable redis
+              EOF
+}
+
+output "redis_public_ip" {
+  value = aws_instance.redis_server.public_ip
+}
